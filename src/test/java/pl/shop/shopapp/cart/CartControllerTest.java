5227532e22eeb9884017069380b44a610a5301a4
ltjakub <jkub.sliwinski@gmail.com>
2023-01-06 21:42:43 +0100

    added more tests

--- src/test/java/pl/shop/shopapp/cart/CartControllerTest.java
+++ src/test/java/pl/shop/shopapp/cart/CartControllerTest.java
@@ -1,25 +1,30 @@
 package pl.shop.shopapp.cart;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class CartControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private CartRepository repository;
 
     @Test
     public void shouldCreateCart() throws Exception {
-        mockMvc.perform(post("/api/createcart/1"))
-                .andExpect(status().isNoContent());
+        MockHttpServletResponse response = mockMvc.perform(post("/api/createcart/3"))
+                .andReturn()
+                .getResponse();
+        boolean isCreated = repository.findById(3L).isPresent();
+        assertThat(isCreated).isTrue();
+        assertThat(response.getStatus()).isEqualTo(204);
     }
 }
--- src/test/java/pl/shop/shopapp/product/ProductControllerTest.java
+++ src/test/java/pl/shop/shopapp/product/ProductControllerTest.java
@@ -1,71 +1,88 @@
 package pl.shop.shopapp.product;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
-
-
+import pl.shop.shopapp.utils.JsonConverter;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.BDDMockito.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-
+import java.util.Optional;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 class ProductControllerTest {
+
     @Autowired
     private MockMvc mockMvc;
-    @MockBean(ProductRepository.class)
-    ProductRepository repository;
-    @MockBean(ProductService.class)
-    ProductService service;
+    @Autowired
+    private ProductRepository repository;
+
 
     @Test
-    public void shouldFindAllProducts() throws Exception {
-        Product product1 = new Product(1L, "Book", "Best book in the world", 5.5);
-        Product product2 = new Product(2L, "Phone", "Best phone in the world", 500.0);
-        List<Product> products = new ArrayList<>();
-        products.add(product1);
-        products.add(product2);
-        given(repository.findAll()).willReturn(products);
-
+    public void shouldReturnAllProducts() throws Exception {
         MockHttpServletResponse response = mockMvc.perform(get("/api/product")).andReturn().getResponse();
         assertThat(response.getStatus()).isEqualTo(200);
-        assertThat(response.getContentAsString()).isNotEmpty();
+        assertThat(response.getContentAsString()).isNotNull();
     }
 
     @Test
-    public void shouldFindProductById() throws Exception {
-        Optional<ProductDto> product = Optional.of(new ProductDto(1L, "Book", "Best book in the world", 5.5));
-        given(service.findProductById(1L)).willReturn(product);
+    public void shouldReturn3ProductsOnThePage() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get("/api/product/page/0")).andReturn().getResponse();
+        System.out.println(response.getContentAsString());
+        assertThat(response.getStatus()).isEqualTo(200);
+        assertThat(response.getContentAsString()).contains("\"id\":1");
+        assertThat(response.getContentAsString()).doesNotContain("\"id\":4");
+    }
 
+    @Test
+    public void shouldReturnProductById() throws Exception {
         MockHttpServletResponse response = mockMvc.perform(get("/api/product/1")).andReturn().getResponse();
         assertThat(response.getStatus()).isEqualTo(200);
-        assertThat(response.getContentAsString()).isNotEmpty();
     }
 
     @Test
-    public void shouldNOTFindProductById() throws Exception {
-        MockHttpServletResponse response = mockMvc.perform(get("/api/product/1")).andReturn().getResponse();
+    public void shouldNOTReturnProductById() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get("/api/product/100")).andReturn().getResponse();
         assertThat(response.getStatus()).isEqualTo(404);
-        assertThat(response.getContentAsString()).isEmpty();
     }
 
     @Test
-    public void shouldDeleteProduct(){
-        repository.deleteById(1L);
-        Mockito.verify(repository, Mockito.times(1)).deleteById(eq(1L));
+    public void shouldAddNewProduct() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(
+                post("/api/product")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(JsonConverter.convertToJson((new Product(6L, "Shovel", "Dig it", 50.0))))
+        ).andReturn().getResponse();
+        assertThat(response.getStatus()).isEqualTo(201);
+        assertThat(response.getRedirectedUrl()).isEqualTo("http://localhost/api/product/6");
+    }
+
+    @Test
+    public void shouldDeleteProduct() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(delete("/api/product/4")).andReturn().getResponse();
+        Optional<Product> deletedProduct = repository.findById(4L);
+        assertThat(deletedProduct.isEmpty()).isTrue();
+        assertThat(response.getStatus()).isEqualTo(204);
+    }
+
+    @Test
+    public void shouldUpdateProductName() throws Exception {
+        String newName = "Brush";
+        MockHttpServletResponse response = mockMvc.perform(
+                        patch("/api/product/2")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content("{\"name\":\"" + newName + "\"}")
+                )
+                .andReturn()
+                .getResponse();
+        Product product = repository.findById(2L).orElseThrow();
+        assertThat(product.getName()).isEqualTo(newName);
+        assertThat(response.getStatus()).isEqualTo(204);
+
     }
 
 }
